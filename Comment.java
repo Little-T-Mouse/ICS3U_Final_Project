67f4a7857dd88065003cb3a27b9b97bfe3116ac1
Tommy Shan <92545115+Little-T-Mouse@users.noreply.github.com>
2023-05-24 13:24:07 -0400

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -7,6 +7,14 @@ public class Comment {
     private int vote;
     private boolean isPublic;
 
+    /**
+     * Constructor for class Comment
+     * @param content the content of the comment
+     * @param author the user that authored the comment
+     * @param date the date that the comment is posted
+     * @param vote the number of vote of the comment
+     * @param isPublic the publicity of the comment
+     */
     public Comment(String content, User author, Date date, int vote, boolean isPublic) {
         setContent(content);
         setAuthor(author);
@@ -15,42 +23,92 @@ public class Comment {
         setPublic(isPublic);
     }
 
+    /**
+     * getContent
+     * Getter method for variable content
+     * @return variable content
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * setContent
+     * Setter method for variable content
+     * @param content variable content
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * isPublic
+     * Getter method for variable isPublic
+     * @return variable isPublic
+     */
     public boolean isPublic() {
         return isPublic;
     }
 
-    public void setPublic(boolean aPublic) {
-        isPublic = aPublic;
+    /**
+     * setPublic
+     * Setter method for variable isPublic
+     * @param isPublic variable isPublic
+     */
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
     }
 
+    /**
+     * getVote
+     * Getter method for variable vote
+     * @return variable vote
+     */
     public int getVote() {
         return vote;
     }
 
+    /**
+     * setVote
+     * Setter method for variable vote
+     * @param vote variable vote
+     */
     public void setVote(int vote) {
         this.vote = vote;
     }
 
+    /**
+     * getDate
+     * Getter method for variable date
+     * @return variable date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * setDate
+     * Setter method for variable date
+     * @param date variable date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * getAuthor
+     * Getter method for variable author
+     * @return variable author
+     */
     public User getAuthor() {
         return author;
     }
 
+    /**
+     * setAuthor
+     * Setter method for variable author
+     * @param author variable author
+     */
     public void setAuthor(User author) {
         this.author = author;
     }
